ea7db40ff96099a3e5df62fa23182b001d904b81
Stanislav Markov <misticosmisticos@gmail.com>
2019-06-29 10:36:49 +0300

    Initial

--- src/test/java/selenium_tests/FunctionalTest.java
+++ src/test/java/selenium_tests/FunctionalTest.java
@@ -1,4 +1,36 @@
 package selenium_tests;
 
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Handles setup and teardown of WebDriver.
+ */
 public class FunctionalTest {
+
+    protected static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\misticos\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void cleanUp(){
+        driver.manage().deleteAllCookies();
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        driver.close();
+    }
 }
