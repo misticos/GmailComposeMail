e9fa83f2883764bf00b978675b65cd018ef2f08c
Stanislav Markov <misticosmisticos@gmail.com>
2019-06-29 14:49:33 +0300

    Initial.

--- src/test/java/selenium_tests/InboxPage.java
+++ src/test/java/selenium_tests/InboxPage.java
@@ -1,4 +1,49 @@
 package selenium_tests;
 
-public class InboxPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class InboxPage extends PageObject{
+
+    @FindBy(className = "Z0")
+    private WebElement Compose;
+
+    @FindBy(className = "v0")
+    private WebElement FieldTo;
+
+    @FindBy(className = ":9t")
+    private WebElement FieldSubject;
+
+    @FindBy(className = ":ay")
+    private WebElement FieldBody;
+
+    public InboxPage(WebDriver driver) {
+        super(driver);
+    }
+
+    //User to troubleshoot
+    public boolean isInitialized(){
+        return Compose.isDisplayed();
+    }
+
+    public void newMail() {
+        this.Compose.click();
+    }
+
+    public void fillToField(String toField) {
+        this.FieldTo.sendKeys(toField);
+    }
+
+    public void fillSubjectField(String subject) {
+        this.FieldTo.sendKeys(subject);
+    }
+
+    public void fillBodyOfEmail(String subject) {
+        this.FieldTo.sendKeys(subject);
+    }
+
+    /*public String confirmationInboxFolder(){
+        return inboxFolder.getText();
+    }*/
 }
--- src/test/java/selenium_tests/LoginPage.java
+++ src/test/java/selenium_tests/LoginPage.java
@@ -1,4 +1,51 @@
 package selenium_tests;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class LoginPage extends PageObject{
+
+    @FindBy(id="identifierId")
+    private WebElement userEmail;
+
+    @FindBy(xpath = "//*[@name=\"password\"]")
+    private WebElement userPassword;
+
+    @FindBy(className = "CwaK9")
+    private WebElement nextButtonOnEmail;
+
+    @FindBy(className = "CwaK9")
+    private WebElement nextButtonOnPassword;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isInitialized() {
+        return userEmail.isDisplayed();
+    }
+
+    public boolean isPasswordInitialized() {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement password = wait.until(ExpectedConditions.elementToBeClickable(userPassword));
+        return userPassword.isDisplayed();
+    }
+
+    public void enterUserEmail(String userEmail) {
+        this.userEmail.clear();
+        this.userEmail.sendKeys(userEmail);
+        nextButtonOnEmail.click();
+    }
+
+    public void enterUserPassword(String userPassword) {
+        this.userPassword.sendKeys(userPassword);
+    }
+
+    public InboxPage nextOnPassword(){
+        nextButtonOnPassword.click();
+        return new InboxPage(driver);
+    }
 }
