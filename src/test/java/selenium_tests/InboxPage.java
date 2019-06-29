47f9c8d848b5ee5e14ecd5eff1c9fc8f25dacfe8
Stanislav Markov <misticosmisticos@gmail.com>
2019-06-29 18:15:41 +0300

    Initial

--- src/test/java/selenium_tests/InboxPage.java
+++ src/test/java/selenium_tests/InboxPage.java
@@ -3,20 +3,33 @@ package selenium_tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class InboxPage extends PageObject{
 
-    @FindBy(className = "Z0")
-    private WebElement Compose;
+    @FindBy(xpath = "//*[@class=\"z0\"]")
+    private WebElement compose;
 
-    @FindBy(className = "v0")
+    @FindBy(xpath = "//textarea[@name='to']")
     private WebElement FieldTo;
 
-    @FindBy(className = ":9t")
+    @FindBy(xpath = "//input[@name='subjectbox']")
     private WebElement FieldSubject;
 
-    @FindBy(className = ":ay")
+    @FindBy(xpath = "//div[@class='Ar Au']//div")
     private WebElement FieldBody;
+
+    @FindBy(linkText = "Send")
+    //div[@class='T-I J-J5-Ji aoO v7 T-I-atl L3']")
+    //Previous xpath"//div[@class='dC']//div")
+    private WebElement sendButton;
+
+    @FindBy(xpath = "//*[@title='Sent']")
+    private WebElement sentFolder;
+
+    @FindBy(partialLinkText = "Hello Appium Automation Course from")
+    private WebElement subjectOfSentEmail;
 
     public InboxPage(WebDriver driver) {
         super(driver);
@@ -24,11 +37,13 @@ public class InboxPage extends PageObject{
 
     //User to troubleshoot
     public boolean isInitialized(){
-        return Compose.isDisplayed();
+        return compose.isDisplayed();
     }
 
     public void newMail() {
-        this.Compose.click();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement composeButton = wait.until(ExpectedConditions.elementToBeClickable(compose));
+        this.compose.click();
     }
 
     public void fillToField(String toField) {
@@ -36,13 +51,24 @@ public class InboxPage extends PageObject{
     }
 
     public void fillSubjectField(String subject) {
-        this.FieldTo.sendKeys(subject);
+        this.FieldSubject.sendKeys(subject);
     }
 
     public void fillBodyOfEmail(String subject) {
-        this.FieldTo.sendKeys(subject);
+        this.FieldBody.sendKeys(subject);
     }
 
+    public void sendButton(){
+        this.sendButton.click();
+    }
+
+    public void sentFolder(){
+        this.sentFolder.click();
+    }
+
+    public String summarySentEmail(){
+        return subjectOfSentEmail.getText();
+    }
     /*public String confirmationInboxFolder(){
         return inboxFolder.getText();
     }*/
