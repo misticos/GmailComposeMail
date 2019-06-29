cd6fcf8cfaa33da49c8a341283a5666ddc4bd5bd
Stanislav Markov <misticosmisticos@gmail.com>
2019-06-29 21:19:10 +0300

    Initial

--- src/test/java/selenium_tests/FunctionalTest.java
+++ src/test/java/selenium_tests/FunctionalTest.java
@@ -7,7 +7,6 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Handles setup and teardown of WebDriver.
